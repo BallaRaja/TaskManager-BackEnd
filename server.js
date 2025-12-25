9d283f2e43d5320b5ba4d928c7c053027da1e66e
BallaRaja <ballaraja77@gmai.com>
2025-12-26 01:14:53 +0530

    Add Port

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
