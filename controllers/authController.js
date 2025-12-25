531891a25c87facebc17c535ae7a274eb63cf776
BallaRaja <ballaraja77@gmai.com>
2025-12-26 01:05:24 +0530

    Basic Server Logic

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await User.create({ email, password: hashed });
+
+  res.json({ message: "User registered" });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ error: "User not found" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ error: "Invalid password" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.json({ token });
+};
