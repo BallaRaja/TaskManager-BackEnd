2bca6ac2d4a5501ebafeb0009fdd7cf6f7c52052
Kuladeep-Reddy-C <ckuladeepreddy2006@gmail.com>
2026-03-04 01:52:13 +0530

    Rate Increased

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 // 🕒 Rate Limiting
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per window
+  max: 10000, // Limit each IP to 100 requests per window
   message: "Too many requests from this IP, please try again after 15 minutes"
 });
 app.use(generalLimiter);
